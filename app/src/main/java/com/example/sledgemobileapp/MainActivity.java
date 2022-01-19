d2edcae28929f7339cc1ace04800d029ba93ce63
Bradley Chu <bradleycchu@gmail.com>
2022-01-18 20:53:39 -0800

    Initial commit

---
+++ app/src/main/java/com/example/sledgemobileapp/MainActivity.java
@@ -0,0 +1,21 @@
+package com.example.sledgemobileapp;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+    public void bluetoothCheck(View view) {
+        TextView textView = findViewById(R.id.textView);
+        textView.setText("Hello!");
+    }
+}
