3de3de61210274e9f1a661cf2832d61b7eeba928
Bradley Chu <bradleycchu@gmail.com>
2022-01-31 20:35:51 -0800

    Added some needed classes for Animations

---
+++ app/src/main/java/com/example/sledgemobileapp/PresetClass.java
@@ -0,0 +1,10 @@
+package com.example.sledgemobileapp;
+
+import java.io.Serializable;
+import java.util.ArrayList;
+
+public class PresetClass implements Serializable {
+    public String name;
+    public ArrayList<animationInfo> animations;
+    public ArrayList<String> animationNames;
+}
