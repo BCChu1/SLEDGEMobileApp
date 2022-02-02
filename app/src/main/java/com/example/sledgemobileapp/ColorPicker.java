258eafac89ac7178a7a7b14773819bb93e6219d2
ntrudeau <ntrudeau@uci.edu>
2022-02-01 22:21:55 -0800

    added colorPicker from GFG, and retrieved hex value of color for display text

--- app/src/main/java/com/example/sledgemobileapp/ColorPicker.java
+++ app/src/main/java/com/example/sledgemobileapp/ColorPicker.java
@@ -1,15 +1,133 @@
 package com.example.sledgemobileapp;
 
 import androidx.appcompat.app.AppCompatActivity;
-
 import android.os.Bundle;
+import android.graphics.Color;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+import androidx.appcompat.app.AppCompatActivity;
+import top.defaults.colorpicker.ColorPickerPopup;
 
 public class ColorPicker extends AppCompatActivity {
+
+    // text view variable to set the color for GFG text
+    private TextView gfgTextView;
+
+    private TextView hexColorTextView;
+
+    // two buttons to open color picker dialog and one to
+    // set the color for GFG text
+    private Button mSetColorButton, mPickColorButton;
+
+    // view box to preview the selected color
+    private View mColorPreview;
+
+    // this is the default color of the preview box
+    private int mDefaultColor;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
 
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_color_picker);
+
+        // register the GFG text with appropriate ID
+        gfgTextView = findViewById(R.id.gfg_heading);
+
+        // register the GFG text with appropriate ID
+        hexColorTextView = findViewById(R.id.hexColorText);
+
+        // register two of the buttons with their
+        // appropriate IDs
+        mPickColorButton = findViewById(R.id.pick_color_button);
+        mSetColorButton = findViewById(R.id.set_color_button);
+
+        // and also register the view which shows the
+        // preview of the color chosen by the user
+        mColorPreview = findViewById(R.id.preview_selected_color);
+
+        // set the default color to 0 as it is black
+        mDefaultColor = 0;
+
+        // handling the Pick Color Button to open color
+        // picker dialog
+        mPickColorButton.setOnClickListener(
+                new View.OnClickListener() {
+                    @Override
+                    public void onClick(final View v) {
+                        new ColorPickerPopup.Builder(ColorPicker.this).initialColor(
+                                Color.RED) // set initial color
+                                // of the color
+                                // picker dialog
+                                .enableBrightness(
+                                        true) // enable color brightness
+                                // slider or not
+                                .enableAlpha(
+                                        true) // enable color alpha
+                                // changer on slider or
+                                // not
+                                .okTitle(
+                                        "CHOOSE") // this is top right
+                                // Choose button
+                                .cancelTitle(
+                                        "CANCEL") // this is top left
+                                // Cancel button which
+                                // closes the
+                                .showIndicator(
+                                        true) // this is the small box
+                                // which shows the chosen
+                                // color by user at the
+                                // bottom of the cancel
+                                // button
+                                .showValue(
+                                        true) // this is the value which
+                                // shows the selected
+                                // color hex code
+                                // the above all values can be made
+                                // false to disable them on the
+                                // color picker dialog.
+                                .build()
+                                .show(
+                                        v,
+                                        new ColorPickerPopup.ColorPickerObserver() {
+                                            @Override
+                                            public void
+                                            onColorPicked(int color) {
+                                                // set the color
+                                                // which is returned
+                                                // by the color
+                                                // picker
+                                                mDefaultColor = color;
+
+                                                // now as soon as
+                                                // the dialog closes
+                                                // set the preview
+                                                // box to returned
+                                                // color
+                                                mColorPreview.setBackgroundColor(mDefaultColor);
+                                            }
+                                        });
+                    }
+                });
+
+        // handling the Set Color button to set the selected
+        // color for the GFG text.
+        mSetColorButton.setOnClickListener(
+                new View.OnClickListener() {
+                    @Override
+                    public void onClick(View v) {
+                        // now change the value of the GFG text
+                        // as well.
+                        gfgTextView.setTextColor(mDefaultColor);
+
+                        String hexColor = "#" + Integer.toHexString(mDefaultColor);
+                        hexColorTextView.setText(hexColor);
+
+                    }
+                });
     }
+
+
 }
